d914b920ea1237af21046ca9e6b487397627c99d
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2021-05-06 09:26:28 +0200

    Start

---
+++ evaluator.py
@@ -0,0 +1,67 @@
+from typing import Dict, Set, List
+
+
+class Evaluator:
+
+    @staticmethod
+    def evaluate_map(predictions: Dict[str, List], ground_truth: Dict[str, Set]):
+        '''
+        Computes the mAP (https://jonathan-hui.medium.com/map-mean-average-precision-for-object-detection-45c121a31173) of the predictions with respect to the given ground truth
+        In person reidentification mAP refers to the mean of the AP over all queries.
+        The AP for a query is the area under the precision-recall curve obtained from the list of predictions considering the
+        ground truth elements as positives and the other ones as negatives
+
+        :param predictions: dictionary from query filename to list of test image filenames associated with the query ordered
+                            from the most to the least confident prediction.
+                            Represents the predictions to be evaluated.
+        :param ground_truth: dictionary from query filename to set of test image filenames associated with the query
+                             Represents the ground truth on which to evaluate predictions.
+
+        :return:
+        '''
+
+        m_ap = 0.0
+        for current_ground_truth_query, current_ground_truth_query_set in ground_truth.items():
+
+            # No predictions were performed for the current query, AP = 0
+            if not current_ground_truth_query in predictions:
+                continue
+
+            current_ap = 0.0  # The area under the curve for the current sample
+            current_predictions_list = predictions[current_ground_truth_query]
+
+            # Recall increments of this quantity each time a new correct prediction is encountered in the prediction list
+            delta_recall = 1.0 / len(current_ground_truth_query_set)
+
+            # Goes through the list of predictions
+            encountered_positives = 0
+            for idx, current_prediction in enumerate(current_predictions_list):
+                # Each time a positive is encountered, compute the current precition and the area under the curve
+                # since the last positive
+                if current_prediction in current_ground_truth_query_set:
+                    encountered_positives += 1
+                    current_precision = encountered_positives / (idx + 1)
+                    current_ap += current_precision * delta_recall
+
+            m_ap += current_ap
+
+        # Compute mean over all queries
+        m_ap /= len(ground_truth)
+
+        return m_ap
+
+
+if __name__ == "__main__":
+
+    predictions = {
+        "a": ["a", "bb", "c", "l"],
+        "f": ["f", "gg", "h"]
+    }
+
+    ground_truth = {
+        "a": set(["a", "b", "c"]),
+        "f": set(["f", "g", "h"])
+    }
+
+    m_ap = Evaluator.evaluate_map(predictions, ground_truth)
+    print(m_ap)
